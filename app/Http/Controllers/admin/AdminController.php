dcec69d7e5a115ff6c3e57b46529122c1e065339
tasnim5sanzida@gmail.com <tasnim5sanzida@gmail.com>
2023-07-09 03:47:50 +0600

    admin profile updated & stored in database

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,5 +26,35 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('admin/login');
+    }
+    public function adminProfile()
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        return view('admin.admin_profile_view',compact('user'));
+    }
+    public function adminProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $user = User::find($id);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+
+        if($request->file('photo'))
+        {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$user->photo));
+            $fileName = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $fileName);
+            $user->photo = $fileName;
+        }
+        $user->save();
+
+        return redirect()->back();
+
     }
 }
