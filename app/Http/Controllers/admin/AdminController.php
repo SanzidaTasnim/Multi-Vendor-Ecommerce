c1ad4046f9d9dc0ea52a53b13676b6cab079abd2
tasnim5sanzida@gmail.com <tasnim5sanzida@gmail.com>
2023-07-09 00:31:50 +0600

    admin login and dashboard done

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function adminDashboard()
     {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    }
+    public function adminLogin()
+    {
+        return view('admin.admin_login');
+    }
+    public function adminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('admin/login');
     }
 }
